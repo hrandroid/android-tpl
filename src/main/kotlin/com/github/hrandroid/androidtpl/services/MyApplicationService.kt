f08f91564f868e2f16e5a9faed146629f6c06800
GitHub Action <action@github.com>
2021-10-08 06:50:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/hrandroid/androidtpl/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hrandroid/androidtpl/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hrandroid.androidtpl.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hrandroid.androidtpl.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/hrandroid/androidtpl/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hrandroid/androidtpl/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hrandroid.androidtpl.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hrandroid.androidtpl.MyBundle
 
 class MyProjectService(project: Project) {
 
